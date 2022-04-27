38423cc00187e071d884b9b4d9479548859953a7
Ane <anebzt@protonmail.com>
2022-04-27 16:06:34 +0200

    added frontend code

---
+++ frontend/src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios'
+
+//function App() {
+class App extends Component {
+
+  state = {
+    // Initially, no file is selected
+    selectedFile: null,
+    fileUploadedSuccessfully: false,
+    APIResult: ""
+  };
+
+  onFileChange = event => {
+    this.setState({ selectedFile: event.target.files[0] });
+  }
+
+  onFileUpload = () => {
+    const formData = new FormData();
+    formData.append(
+      "demo files",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    )
+    // call api to upload file
+    // `${process.env.REACT_APP_ENDPOINT}step1`
+    axios.post(`https://19c2sjcjb2.execute-api.eu-central-1.amazonaws.com/prod/step1`, formData).then(response => {
+      console.log(response.data.result);
+      this.setState({ selectedFile: false })
+      this.setState({ fileUploadedSuccessfully: true });
+    })
+  }
+
+  fileData = () => {
+    if (this.state.selectedFile) {
+      return (
+        <div>
+          <h2>File Details:</h2>
+          <p>File Name: {this.state.selectedFile.name}</p>
+          <p> File Type: {this.state.selectedFile.type}</p>
+        </div>
+      )
+    } else if (this.state.fileUploadedSuccessfully) {
+      return (
+        <div>
+          <br />
+          <h4>Your file has been successfully uploaded</h4>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <br />
+          <h4> Choose a file and press the Upload button</h4>
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Translate subtitle files 🎥</h1>
+          <div>
+            <h3>Upload your file here</h3>
+            <div>
+              <input type="file" onChange={this.onFileChange} />
+              <button onClick={this.onFileUpload}>Upload</button>
+            </div>
+            {this.fileData()}
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
